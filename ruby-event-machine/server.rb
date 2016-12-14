46f685435a2a922ddd1fb4df3ec1bd7232635a1b
Oleg G.Kapranov <oleg@vertiopia.com>
2016-12-14 11:24:16 +0200

    added EM to server.rb

--- ruby-event-machine/server.rb
+++ ruby-event-machine/server.rb
@@ -15,4 +15,26 @@ OptionParser.new do |opts|
   opts.on("-p", "--port PORT", Integer, "Port") do |p|
     port = Integer(p)
   end
+
 end.parse!
+
+EM.epoll
+EM.run {
+  @channel = EM::Channel.new
+
+  EM::WebSocket.run(:host => address, :port => port) do |ws|
+    ws.onopen {
+      @channel.subscribe {|msg| ws.send msg }
+    }
+
+    ws.onmessage { |msg|
+      cmd, payload = JSON(msg).values_at('type', 'payload')
+      if cmd == 'echo'
+        ws.send({type: 'echo', payload: payload}.to_json)
+      else
+        @channel.push({type: 'broadcast', payload: payload}.to_json)
+        ws.send({type: "broadcastResult", payload: payload}.to_json)
+      end
+    }
+  end
+}
